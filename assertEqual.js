e2fc1f63a84a129f394f7e6b0fe0964c79c7c699
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-06-28 01:12:17 +0000

    add emoji

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x1F600) + "Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(String.fromCodePoint(0x274E) + "Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("aaa", 1);
+assertEqual(1, 5);
+assertEqual("1", "1");

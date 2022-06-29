e2166d2bfee8c35db2d54986b5613cb26bda651d
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-06-29 00:58:06 +0000

    use array prototype instead of for loop

--- without.js
+++ without.js
@@ -20,14 +20,15 @@ const assertArraysEqual = function(arr1, arr2) {
 
 };
 
+const without = function(source, itemsToRemove) {
+  return source.filter(x => !itemsToRemove.includes(x));
+};
 
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
 
-
-//TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 6], [1, 2, 3]);
-assertArraysEqual(['1', '2', '3'], [1, 2, 3]);
-assertArraysEqual([1, 2], [1, 2, 3]);
-
-
-
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+console.log(words);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);

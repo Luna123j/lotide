7499a8871160c29c884977eeb6e0fb5806e9024c
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-06-30 20:27:48 +0000

    add findkey

---
+++ findkey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x1F600)} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x274E)} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, action) {
+  for (const item in obj) {
+    if (action(obj[item])) {
+      return item;
+    }
+  }
+};
+
+
+let answer = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(answer,'noma')

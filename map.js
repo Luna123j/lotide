8d68ecd1f4da162b738e6c66a0a2e24364d6957d
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-06-30 19:55:55 +0000

    add map

---
+++ map.js
@@ -0,0 +1,12 @@
+//The map function will return a new array based on the results of the callback function.
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+console.log(results1);

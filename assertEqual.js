c3f41a017867709135291ece590761737cf45866
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-07-04 18:01:27 +0000

    add test case

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,14 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`${String.fromCodePoint(0x1F600)} Assertion Passed: ${actual} === ${expected}`);
+    console.log(`😃   Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`${String.fromCodePoint(0x274E)} Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`❌  Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
 module.exports = assertEqual;
+
 // TEST CODE
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual("aaa", 1);

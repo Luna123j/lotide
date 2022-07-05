25f1db9d8985861420b10daa0de47620b82e6f17
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-07-05 02:18:22 +0000

    edit readme

--- README.md
+++ README.md
@@ -29,3 +29,9 @@ The following functions are currently implemented:
 * `head(array)`: return index 0 of an array
 * `tail(array)`: return an array without index 0
 * `middle(array)`: return middle value of an array
+* `flatten (array)`: return flatten array 
+* `countOnly(allItems, itemsToCount)`: return count number of item(obj) in the given items(obj)
+* `letterPositions(sentence)`: return letter position in an sentence
+* `findKeyByValue(object,objectKeyValue)`: return key from an given key value
+* `eqObjects(obj,obj)`: return true if two objects are same
+* `eqArrays(arr,arr)`: return true if two arrays are same
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // index.js
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
+const countOnly = require("./countOnly");
+const letterPositions = require("./letterPositions");
+const flatten = require("./flatten");
+const findKeyByValue = require("./findKeyByValue");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
 
 module.exports = {
   head,
   tail,
-  middle
+  middle,
+  flatten,
+  countOnly,
+  letterPositions,
+  findKeyByValue,
+  eqObjects,
+  eqArrays,
 };

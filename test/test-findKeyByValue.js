90f162e9dbccc710e4b93283a2753b469733742d
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-07-05 01:56:30 +0000

    edit test case for findKeyByValue

---
+++ test/test-findKeyByValue.js
@@ -0,0 +1,30 @@
+const findKeyByValue = require("../findKeyByValue");
+const expect = require("chai").expect;
+
+describe("Test eqArrays.js", () => {
+
+  it("should return its key for specific key value", () => {
+    const bestTVShowsByGenre = {
+      // eslint-disable-next-line camelcase
+      sci_fi: "The Expanse",
+      comedy: "Brooklyn Nine-Nine",
+      drama: "The Wire"
+    };
+    const result = findKeyByValue(bestTVShowsByGenre, "The Wire");
+    const expectResult = "drama";
+    expect(result).to.equal(expectResult);
+  });
+
+  it("should return undefined if the specific key value not exist", () => {
+    const bestTVShowsByGenre = {
+      // eslint-disable-next-line camelcase
+      sci_fi: "The Expanse",
+      comedy: "Brooklyn Nine-Nine",
+      drama: "The Wire"
+    };
+    const result = findKeyByValue(bestTVShowsByGenre, "That '70s Show");
+    const expectResult = undefined;
+    expect(result).to.equal(expectResult);
+  });
+
+});
